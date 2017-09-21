380dd503f82660f4e63ab13cf905b4f83ae9098a
sky <1062855360@qq.com>
2017-09-21 20:36:50 +0800

    modify

---
+++ BookShop/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookShop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("用户名不能为空！");
+                return;
+            }
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("密码不能为空！");
+            }
+            if (textBox2.Text == "1" && textBox1.Text == "1")
+            {
+                 index index = new index();
+                index.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox .Show("用户名或密码错误！");
+            }
+        }
+    }
+}
---
+++ BookShop/index.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace BookShop
+{
+    partial class index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.基本档案ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.进货管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.销售管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.库存管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.问题ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.menuStrip1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.基本档案ToolStripMenuItem,
+            this.进货管理ToolStripMenuItem,
+            this.销售管理ToolStripMenuItem,
+            this.库存管理ToolStripMenuItem,
+            this.问题ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(707, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 基本档案ToolStripMenuItem
+            // 
+            this.基本档案ToolStripMenuItem.Name = "基本档案ToolStripMenuItem";
+            this.基本档案ToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.基本档案ToolStripMenuItem.Text = "基本档案";
+            this.基本档案ToolStripMenuItem.Click += new System.EventHandler(this.基本档案ToolStripMenuItem_Click);
+            // 
+            // 进货管理ToolStripMenuItem
+            // 
+            this.进货管理ToolStripMenuItem.Name = "进货管理ToolStripMenuItem";
+            this.进货管理ToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.进货管理ToolStripMenuItem.Text = "进货管理";
+            // 
+            // 销售管理ToolStripMenuItem
+            // 
+            this.销售管理ToolStripMenuItem.Name = "销售管理ToolStripMenuItem";
+            this.销售管理ToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.销售管理ToolStripMenuItem.Text = "销售管理";
+            // 
+            // 库存管理ToolStripMenuItem
+            // 
+            this.库存管理ToolStripMenuItem.Name = "库存管理ToolStripMenuItem";
+            this.库存管理ToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.库存管理ToolStripMenuItem.Text = "库存管理";
+            // 
+            // 问题ToolStripMenuItem
+            // 
+            this.问题ToolStripMenuItem.Name = "问题ToolStripMenuItem";
+            this.问题ToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.问题ToolStripMenuItem.Text = "问题";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 353);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(707, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "当前操作员";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(80, 17);
+            this.toolStripStatusLabel1.Text = "当前操作员：";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(68, 17);
+            this.toolStripStatusLabel3.Text = "当前日期：";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel4.Text = "toolStripStatusLabel4";
+            // 
+            // index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(707, 375);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "index";
+            this.Text = "e家图书系统";
+            this.Load += new System.EventHandler(this.index_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 基本档案ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 进货管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 销售管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 库存管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 问题ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+    }
+}
